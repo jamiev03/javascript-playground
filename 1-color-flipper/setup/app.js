b0131f05e28d2b0c31044ae1e02ea75251ba43ea
Jamie <jamie03.v@gmail.com>
2021-04-08 21:21:16 -0400

    add basic html template and begin work on app.js

--- 1-color-flipper/setup/app.js
+++ 1-color-flipper/setup/app.js
@@ -1,1 +1,10 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const btn = document.getElementById('btn');
+const color = document.querySelector('.color');
+
+//set up event listener on the button
+btn.addEventListener('click', function() {
+    //get a random number between 0 - 3 since colors array is 0 - 3
+    const randomNumber = Math.floor(Math.random() * 3) + 1;
+    console.log(randomNumber);
+})
