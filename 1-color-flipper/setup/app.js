61d75abb3b97d51b64e0a6ed60b58996e541ad8a
Jamie <jamie03.v@gmail.com>
2021-04-09 20:36:23 -0400

    moved event listener to an outside function

--- 1-color-flipper/setup/app.js
+++ 1-color-flipper/setup/app.js
@@ -5,6 +5,12 @@ const color = document.querySelector('.color');
 //set up event listener on the button
 btn.addEventListener('click', function() {
     //get a random number between 0 - 3 since colors array is 0 - 3
-    const randomNumber = Math.floor(Math.random() * 3) + 1;
+    const randomNumber = randomNumberGenerator();
     console.log(randomNumber);
+    document.body.style.backgroundColor = colors[randomNumber];
+    color.textContent = colors[randomNumber];
 })
+
+function randomNumberGenerator() {
+    return Math.floor(Math.random() * colors.length) + 1;
+}
