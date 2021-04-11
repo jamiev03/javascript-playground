f42f2c04b776ae5a334dbdd4973c8791d040c8df
Jamie <jamie03.v@gmail.com>
2021-04-10 20:11:23 -0400

    target buttons and apply event listeners. using event and current target.classlist pulled classes available on buttons. using these as ref points with contains()

--- 2-counter/setup/app.js
+++ 2-counter/setup/app.js
@@ -7,6 +7,26 @@ const btns = document.querySelectorAll('.btn');
 
 btns.forEach((button) => {
     button.addEventListener('click', (e) => {
-        console.log(e.currentTarget);
+        const styles = e.currentTarget.classList;
+        if (styles.contains('decrease')) {
+            count--;
+        }
+        else if (styles.contains('increase')) {
+            count++;
+        }
+        else {
+            styles.contains('reset')
+            count = 0;
+        }
+        value.innerText = count;
+        if (count > 0) {
+            value.style.color = 'green';
+        }
+        else if (count < 0) {
+            value.style.color = 'red';
+        }
+        else {
+            value.style.color = '#222222'
+        }
     })
 })
