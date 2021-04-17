30b85ee217403743626b5af00afd375c8b298fa6
Jamie <jamie03.v@gmail.com>
2021-04-16 20:43:09 -0400

    general markup of first question box

--- 6-modal/setup/app.js
+++ 6-modal/setup/app.js
@@ -4,11 +4,12 @@ const modalOverlay = document.querySelector('.modal-overlay');
 const modalCloseBtn = document.querySelector('.close-btn');
 // listen for click events on modal-btn and close-btn
 modalBtn.addEventListener('click', () => {
-
+    console.log('clicked');
+    modalOverlay.classList.add('open-modal');
 })
 
 modalCloseBtn.addEventListener('click', () => {
-    
+    modalOverlay.classList.remove('open-modal')
 })
 // when user clicks modal-btn add .open-modal to modal-overlay
 // when user clicks close-btn remove .open-modal from modal-overlay
