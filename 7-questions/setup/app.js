f7758d56d40ff4fec8510c23e7e73eb472f518ae
Jamie <jamie03.v@gmail.com>
2021-04-17 21:14:10 -0400

    using direct selectors this time to add class to article

--- 7-questions/setup/app.js
+++ 7-questions/setup/app.js
@@ -1,12 +1,31 @@
 //using selectors inside the element
 // traversing the dom
 
-const btns = document.querySelectorAll('.question-btn');
+const questions = document.querySelectorAll('.question');
 
-btns.forEach((btn) => {
-    btn.addEventListener('click', (e) => {
-        const question = e.currentTarget.parentElement.parentElement;
-        console.log(question);
-        question.console.toggle('show-text')
+questions.forEach((q) => {
+    const btn = q.querySelector('.question-btn')
+    btn.addEventListener('click', () => {
+        questions.forEach((item) => {
+            if (item !== q) {
+                item.classList.remove('show-text')
+            }
+        });
+        q.classList.toggle('show-text')
     })
 })
+
+
+
+
+
+
+// const btns = document.querySelectorAll('.question-btn');
+
+// btns.forEach((btn) => {
+//     btn.addEventListener('click', (e) => {
+//         const question = e.currentTarget.parentElement.parentElement;
+//         console.log(question);
+//         question.classList.toggle('show-text')
+//     });
+// });
